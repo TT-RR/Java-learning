b9437b69576d7c758ac6c9ad608901fe2c4b2c1c
TT-RR <kaiza1mage@gmail.com>
2023-06-15 10:15:41 +0900

    メソッド基礎(1)

---
+++ method_1.java
@@ -0,0 +1,16 @@
+package test;
+
+public class Code5_2 {
+
+	public static void main(String[] args) {
+		// メソッドの呼び出し
+		System.out.println("メソッドを呼び出します。");
+		hello();
+		System.out.println("メソッドの呼び出しが終わりました");
+	}
+	
+	public static void hello() {
+		System.out.println("湊さん、こんにちは");
+	}
+
+}
