b08f2868c00ec1c8a1dcb535ebf6df6437ff3185
TT-RR <kaiza1mage@gmail.com>
2023-06-23 11:05:51 +0900

    乱数生成コード

---
+++ scanner/random.java
@@ -0,0 +1,7 @@
+public static void Random() {
+    //整数の乱数を生成
+	int random_int = new java.util.Random().nextInt();
+
+    //小数の乱数を生成
+    double random_double = new java.util.Random().nextDouble();
+}
